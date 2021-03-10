277c70a03776f8b9cdabaef1d36496df1d305f52
Eric-Guo <eric.guocz@gmail.com>
2021-03-10 15:48:28 +0800

    Test on sso-id.com

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -10,14 +10,14 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            scope: %i[openid email profile departments positions main_position],
            response_type: :code,
            uid_field: 'email',
-           issuer: 'https://oauth2id.test',
+           issuer: 'https://sso-id.com',
            discovery: true,
            # Or get signing key from https://oauth2id.test/oauth/discovery/keys && uncomment below 2 line to disable discovery
            # client_signing_alg: :RS256,
            # client_jwk_signing_key: '',
            client_options: {
              scheme: 'https',
-             host: 'oauth2id.test',
+             host: 'sso-id.com',
              identifier: Rails.application.credentials.openid_connect_identifier!,
              secret: Rails.application.credentials.openid_connect_secret!,
              redirect_uri: 'https://omniauth-openid-connect-demo.test/auth/openid_connect/callback',

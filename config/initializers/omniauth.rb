41201c3160fae346e46b24e8e5c668014b206e62
Eric Guo <eric.guocz@gmail.com>
2022-01-19 09:49:36 +0800

    More profile field.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :openid_connect,
            name: :openid_connect,
-           scope: %i[openid departments positions main_position],
+           scope: %i[openid email profile departments positions main_position],
            response_type: :code,
            uid_field: 'email',
            nonce: false,

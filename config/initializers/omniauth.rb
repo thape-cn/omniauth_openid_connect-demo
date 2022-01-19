b003c55c7349c499cab3f0dc96100c10875e778a
Eric Guo <eric.guocz@gmail.com>
2022-01-19 14:17:52 +0800

    Using same setting as google.
    https://docs.gitlab.com/ee/administration/auth/oidc.html#google

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,25 +2,16 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :openid_connect,
-           name: :openid_connect,
-           scope: %i[openid email profile departments positions main_position],
-           response_type: :code,
-           uid_field: 'email',
-           nonce: false,
+           name: 'openid_connect',
+           scope: ['openid', 'profile', 'email'],
+           response_type: 'code',
            issuer: 'https://oauth2id.test',
+           client_auth_method: 'query',
            discovery: true,
-           # Or get signing key from https://oauth2id.test/oauth/discovery/keys && uncomment below 2 line to disable discovery
-           # client_signing_alg: :RS256,
-           # client_jwk_signing_key: '',
+           uid_field: 'email',
            client_options: {
-             scheme: 'https',
-             host: 'oauth2id.test',
              identifier: Rails.application.credentials.openid_connect_identifier!,
              secret: Rails.application.credentials.openid_connect_secret!,
-             redirect_uri: 'https://omniauth-openid-connect-demo.test/auth/openid_connect/callback',
-             authorization_endpoint: '/oauth/authorize',
-             token_endpoint: '/oauth/token',
-             userinfo_endpoint: '/oauth/userinfo',
-             jwks_uri: '/oauth/discovery/keys'
+             redirect_uri: 'https://omniauth-openid-connect-demo.test/auth/openid_connect/callback'
            }
 end
